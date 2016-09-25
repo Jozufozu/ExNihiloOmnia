650a972d50eecc9aa9c06b79303d1e1fa5301fb6
JozsefA <jozsefaug@gmail.com>
2016-09-24 20:21:16 -0700

    Fixed crash when first loading nether

--- src/main/java/exnihiloomnia/world/generation/ChunkProviderVoidHell.java
+++ src/main/java/exnihiloomnia/world/generation/ChunkProviderVoidHell.java
@@ -5,23 +5,29 @@ import static net.minecraftforge.event.terraingen.InitMapGenEvent.EventType.NETH
 import exnihiloomnia.util.helpers.PositionHelper;
 import exnihiloomnia.world.ENOWorld;
 import exnihiloomnia.world.generation.templates.pojos.Template;
+import net.minecraft.entity.EnumCreatureType;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.World;
+import net.minecraft.world.biome.Biome;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkPrimer;
 import net.minecraft.world.gen.ChunkProviderHell;
 import net.minecraft.world.gen.structure.MapGenNetherBridge;
 import net.minecraftforge.event.terraingen.TerrainGen;
 
+import java.util.List;
+
 public class ChunkProviderVoidHell extends ChunkProviderHell {
 	private World world;
-	MapGenNetherBridge fortresses;
+	private MapGenNetherBridge fortresses = new MapGenNetherBridge();
 	
 	public ChunkProviderVoidHell(World world, boolean shouldGenNetherFortress, long seed) {
-		super(world, shouldGenNetherFortress, seed);
+		super(world, false, seed);
 		this.world = world;
-		
-		this.fortresses = (MapGenNetherBridge) TerrainGen.getModdedMapGen(new MapGenNetherBridge(), NETHER_BRIDGE);
+
+		this.fortresses = (MapGenNetherBridge) TerrainGen.getModdedMapGen(fortresses, NETHER_BRIDGE);
 	}
 
 	@Override
@@ -50,9 +56,31 @@ public class ChunkProviderVoidHell extends ChunkProviderHell {
         		template.generate(world, (int)xSpawn, (int)zSpawn);
         	}
         }
-    	
-    	if (ENOWorld.getNetherFortressesAllowed() && fortresses != null) {	
-    		fortresses.generateStructure(world, world.rand, new ChunkPos(x,z));
-        }
+		fortresses.generateStructure(world, world.rand, new ChunkPos(x,z));
+    }
+
+    @Override
+	public List<Biome.SpawnListEntry> getPossibleCreatures(EnumCreatureType creatureType, BlockPos pos)
+	{
+		if (creatureType == EnumCreatureType.MONSTER)
+		{
+			if (this.fortresses.isInsideStructure(pos))
+			{
+				return this.fortresses.getSpawnList();
+			}
+
+			if (this.fortresses.isPositionInStructure(this.world, pos) && this.world.getBlockState(pos.down()).getBlock() == Blocks.NETHER_BRICK)
+			{
+				return this.fortresses.getSpawnList();
+			}
+		}
+
+		Biome biome = this.world.getBiomeGenForCoords(pos);
+		return biome.getSpawnableList(creatureType);
+	}
+
+    public void recreateStructures(Chunk chunkIn, int x, int z)
+    {
+        this.fortresses.generate(this.world, x, z, null);
     }
 }
