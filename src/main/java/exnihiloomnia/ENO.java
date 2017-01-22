2102972f182c39fc3e6cd1c477958451a38e2a48
JozsefA <jozsefaug@gmail.com>
2017-01-22 14:05:15 -0800

    Bump version

--- src/main/java/exnihiloomnia/ENO.java
+++ src/main/java/exnihiloomnia/ENO.java
@@ -55,7 +55,7 @@ public class ENO {
 	
 	public static final String NAME = "Ex Nihilo Omnia";
 	public static final String MODID = "exnihiloomnia";
-	public static final String VERSION = "1.2.5";
+	public static final String VERSION = "1.2.6";
 	public static final String DEPENDENCIES = "after:tconstruct;after:Mekanism;after:IC2;after:appliedenergistics2;after:FunOres;after:forestry;after:morebees;after:immersiveengineering;after:bigreactors;after:substratum";
 	public static final String GUI_FACTORY = "exnihiloomnia.config.ENOGuiFactory";
 
